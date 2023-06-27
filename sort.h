4f56cdfb0d93f5c6dba1cd815ad2e140a709a30a
Matia-k <matia40k@gmail.com>
2023-06-27 15:13:10 -0700

    updated header file

--- sort.h
+++ sort.h
@@ -25,5 +25,7 @@ void insertion_sort_list(listint_t **list);
 void swap_node(listint_t **h, listint_t *a, listint_t *b);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void sort(int *array, size_t size, int f, int l);
+int lomuto(int *array, size_t size, int f, int l);
 
 #endif
